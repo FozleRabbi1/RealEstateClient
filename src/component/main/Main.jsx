50c06d187de8eedc46195d9238f585d0a5aac3a4
FozleRabbi1 <fozlerabbishuvo@gmail.com>
2024-09-04 15:06:59 +0600

    set cursor follower

--- src/component/main/Main.jsx
+++ src/component/main/Main.jsx
@@ -1,17 +1,57 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../shared/nav/Nav";
 import Footer from "../shared/Footer/Footer";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Main = () => {
   const [scrollY, setScrollY] = useState(0);
-    useEffect(() => {
-        const handleScroll = () => setScrollY(window.scrollY);
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
+  useEffect(() => {
+    const handleScroll = () => setScrollY(window.scrollY);
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [delayedPosition, setDelayedPosition] = useState({ x: 0, y: 0 });
+  const [delayedPosition2, setDelayedPosition2] = useState({ x: 0, y: 0 });
+  const requestRef = useRef();
+
+  useEffect(() => {
+    const handleScroll = () => setScrollY(window.scrollY);
+    const handleMouseMove = (e) => setMousePosition({ x: e.clientX, y: e.clientY });
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+
+  useEffect(() => {
+
+    const updateCursor = () => {
+      const delayCursor = (current, target, delay) => current + (target - current) * delay;
+      setDelayedPosition(prev => ({
+        x: delayCursor(prev.x, mousePosition.x, 0.1),
+        y: delayCursor(prev.y, mousePosition.y, 0.1),
+      }));
+
+      setDelayedPosition2(prev => ({
+        x: delayCursor(prev.x, mousePosition.x, 0.5),
+        y: delayCursor(prev.y, mousePosition.y, 0.5),
+      }));
+      requestRef.current = requestAnimationFrame(updateCursor);
+    };
+    requestRef.current = requestAnimationFrame(updateCursor);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, [mousePosition]);
+
+
+
 
   return (
 
@@ -22,6 +62,41 @@ const Main = () => {
         </div>
         <Outlet />
         <Footer />
+
+
+        <div className="hidden lg:block">
+        <div
+          style={{
+            position: "fixed",
+            width: "10px",
+            height: "10px",
+            borderRadius: "50%",
+            backgroundColor: "aqua",
+            pointerEvents: "none",
+            zIndex: 99999999,
+            transform: "translate(-50%, -50%)",
+            left: `${delayedPosition.x}px`,
+            top: `${delayedPosition.y}px`,
+          }}
+          className="custom-cursor"
+        ></div>
+        <div
+          style={{
+            position: "fixed",
+            width: "40px",
+            height: "40px",
+            borderRadius: "50%",
+            border: "1px solid aqua",
+            backgroundColor: "transparent",
+            pointerEvents: "none",
+            zIndex: 9999,
+            transform: "translate(-50%, -50%)",
+            left: `${delayedPosition2.x}px`,
+            top: `${delayedPosition2.y}px`,
+          }}
+          className="custom-cursor-ring"
+        ></div>
+      </div>
       </div>
     </div>
 
--- src/component/shared/Footer/Footer.jsx
+++ src/component/shared/Footer/Footer.jsx
@@ -4,6 +4,10 @@ import "./Footer.css"
 import { FaFacebook, FaTwitter } from "react-icons/fa";
 import { CiInstagram } from "react-icons/ci";
 import { TiSocialLinkedin } from "react-icons/ti";
+import { MdEmail } from "react-icons/md";
+import { IoBag } from "react-icons/io5";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { BiSolidMessageRounded } from "react-icons/bi";
 
 const Footer = () => {
   const spanStyle = "absolute top-0 left-0 h-full w-full border  rounded-md bg-aqua transition-all duration-300"
@@ -13,71 +17,155 @@ const Footer = () => {
 
   return (
     <div id="footer" className="bg-black rounded-xl">
-      <div className="max-w-[1440px] mx-auto py-20 text-white grid grid-cols-2">
-        <div className="w-[55%]">
-          <h2 className="font-normal text-[36px] leading-[45px] tracking-[-1.08px]">
-            Discover exclusive real estate opportunities
-          </h2>
+      <div className="max-w-[1440px] mx-auto py-20 text-white ">
 
-          <p>Lorem ipsum dolor sit amet consectetur. Egestas eu amet dictum tellus. Purus morbi lorem viverra cras.</p>
+        <div className="grid grid-cols-2">
+          <div className="w-[55%]">
+            <h2 className="font-normal text-[36px] leading-[45px] tracking-[-1.08px]">
+              Discover exclusive real estate opportunities
+            </h2>
 
-          <div className="flex mt-4 justify-between">
-            <h2>Enter your email address</h2>
-            <Button title="Subscribe" btnStyle="bg-white text-black flex justify-center items-center flex-row-reverse py-1" iconStyle="bg-white text-red" icon={FaCircleArrowRight} />
+            <p>Lorem ipsum dolor sit amet consectetur. Egestas eu amet dictum tellus. Purus morbi lorem viverra cras.</p>
+
+            <div className="flex mt-4 justify-between items-center bg-[#202127] rounded-full pl-5 pr-1 py-1">
+              <h2>Enter your email address</h2>
+              <Button title="Subscribe" btnStyle="bg-white text-black flex justify-center items-center flex-row-reverse py-1" iconStyle="bg-white text-red" icon={FaCircleArrowRight} />
+            </div>
+
+            <div className="flex ">
+              <div className="icons mt-8 flex space-x-4 mr-5">
+                <a href="#" className="relative w-[40px] h-[40px] group">
+                  <div className={spanDivStyle}>
+                    <span className={`${spanStyle} border-blue-600`}></span>
+                    <span className={`${spanStyle} border-blue-600`}></span>
+                    <span className={`${spanStyle} border-blue-600`}></span>
+                    <span className={`${spanStyle} border-blue-600`}></span>
+                    <FaFacebook className={`group-hover:translate-x-[-2px] text-blue-600 ${iconStyle}`} />
+                  </div>
+                </a>
+              </div>
+
+              <div className="icons mt-8 flex space-x-4 mr-5">
+                <a href="#" className="relative w-[40px] h-[40px] group">
+                  <div className={spanDivStyle}>
+                    <span className={`${spanStyle} border-sky-600`}></span>
+                    <span className={`${spanStyle} border-sky-600`}></span>
+                    <span className={`${spanStyle} border-sky-600`}></span>
+                    <span className={`${spanStyle} border-sky-600`}></span>
+                    <FaTwitter className={`group-hover:translate-x-[-2px] text-sky-600 ${iconStyle}`} />
+                  </div>
+                </a>
+              </div>
+
+              <div className="icons mt-8 flex space-x-4 mr-5">
+                <a href="#" className="relative w-[40px] h-[40px] group">
+                  <div className={spanDivStyle}>
+                    <span className={`${spanStyle} border-orange-600`}></span>
+                    <span className={`${spanStyle} border-orange-600`}></span>
+                    <span className={`${spanStyle} border-orange-600`}></span>
+                    <span className={`${spanStyle} border-orange-600`}></span>
+                    <CiInstagram className={`group-hover:translate-x-[-2px] text-orange-600 ${iconStyle}`} />
+                  </div>
+                </a>
+              </div>
+              <div className="icons mt-8 flex space-x-4">
+                <a href="#" className="relative w-[40px] h-[40px] group">
+                  <div className={spanDivStyle}>
+                    <span className={`${spanStyle} border-blue-500`}></span>
+                    <span className={`${spanStyle} border-blue-500`}></span>
+                    <span className={`${spanStyle} border-blue-500`}></span>
+                    <span className={`${spanStyle} border-blue-500`}></span>
+                    <TiSocialLinkedin className={`group-hover:translate-x-[-2px] text-blue-500 ${iconStyle}`} />
+                  </div>
+                </a>
+              </div>
+            </div>
+
           </div>
+          <div>
+            <h2 className="font-medium text-[20px] leading-[25px] tracking-[-0.6px] mb-2">
+              Main pages
+            </h2>
 
-          <div className="flex ">
-            <div className="icons mt-8 flex space-x-4 mr-5">
-              <a href="#" className="relative w-[40px] h-[40px] group">
-                <div className={spanDivStyle}>
-                  <span className={`${spanStyle} border-blue-600`}></span>
-                  <span className={`${spanStyle} border-blue-600`}></span>
-                  <span className={`${spanStyle} border-blue-600`}></span>
-                  <span className={`${spanStyle} border-blue-600`}></span>
-                  <FaFacebook className={`group-hover:translate-x-[-2px] text-blue-600 ${iconStyle}`} />
-                </div>
-              </a>
-            </div>
 
-            <div className="icons mt-8 flex space-x-4 mr-5">
-              <a href="#" className="relative w-[40px] h-[40px] group">
-                <div className={spanDivStyle}>
-                  <span className={`${spanStyle} border-sky-600`}></span>
-                  <span className={`${spanStyle} border-sky-600`}></span>
-                  <span className={`${spanStyle} border-sky-600`}></span>
-                  <span className={`${spanStyle} border-sky-600`}></span>
-                  <FaTwitter className={`group-hover:translate-x-[-2px] text-sky-600 ${iconStyle}`} />
-                </div>
-              </a>
-            </div>
-
-            <div className="icons mt-8 flex space-x-4 mr-5">
-              <a href="#" className="relative w-[40px] h-[40px] group">
-                <div className={spanDivStyle}>
-                  <span className={`${spanStyle} border-orange-600`}></span>
-                  <span className={`${spanStyle} border-orange-600`}></span>
-                  <span className={`${spanStyle} border-orange-600`}></span>
-                  <span className={`${spanStyle} border-orange-600`}></span>
-                  <CiInstagram className={`group-hover:translate-x-[-2px] text-orange-600 ${iconStyle}`} />
-                </div>
-              </a>
-            </div>
-            <div className="icons mt-8 flex space-x-4">
-              <a href="#" className="relative w-[40px] h-[40px] group">
-                <div className={spanDivStyle}>
-                  <span className={`${spanStyle} border-blue-500`}></span>
-                  <span className={`${spanStyle} border-blue-500`}></span>
-                  <span className={`${spanStyle} border-blue-500`}></span>
-                  <span className={`${spanStyle} border-blue-500`}></span>
-                  <TiSocialLinkedin className={`group-hover:translate-x-[-2px] text-blue-500 ${iconStyle}`} />
-                </div>
-              </a>
+            <div className="ulliStyle">
+              <div className="grid grid-cols-3 ">
+                <ul>
+                  <li>Home </li>
+                  <li>About</li>
+                  <li>Properties</li>
+                  <li>Location CMS</li>
+                  <li>Property CSM</li>
+                  <li>Type CMS Sales</li>
+                  <li>Property single</li>
+                </ul>
+                <ul>
+                  <li>Agents</li>
+                  <li>Agents single</li>
+                  <li>Blog</li>
+                  <li>Blog post</li>
+                  <li>Post a free property</li>
+                  <li>Post a paid property</li>
+                  <li>Contact</li>
+                </ul>
+                <ul>
+                  <li>FAQs</li>
+                  <li>Property Management</li>
+                  <li>Owner Resource Page</li>
+                  <li>Resident Resource Page</li>
+                  <li>Resident Resource Page</li>
+                </ul>
+              </div>
             </div>
           </div>
-
         </div>
-        <div>
-          <h2>Main pages</h2>
+
+
+        <div className="mt-10   grid grid-cols-2">
+          <div>
+            <img src="https://i.ibb.co/S6QFhgf/logoText.png" alt="" />
+            <h2>Powerd by DataSolve </h2>
+          </div>
+
+          <div>
+            <h2 className=" font-medium text-[20px] leading-[25px] tracking-[-0.6px] mb-2">
+              Contact us
+            </h2>
+
+            <div className="grid grid-cols-2 gap-y-3">
+              <span className="flex">
+                <MdEmail className="text-4xl text-white mr-3" />
+                <div>
+                  <h2>Email address</h2>
+                  <h2>info@home.com</h2>
+                </div>
+              </span>
+              <span className="flex">
+                <IoBag className="text-4xl text-white mr-3" />
+                <div>
+                  <h2>Sales executives</h2>
+                  <h2>sales@home.com</h2>
+                </div>
+              </span>
+              <span className="flex">
+                <BsFillTelephoneFill className="text-4xl text-white mr-3" />
+                <div>
+                  <h2>Phone number</h2>
+                  <h2>(123) 456 - 7890</h2>
+                </div>
+              </span>
+              <span className="flex">
+                <BiSolidMessageRounded className="text-4xl text-white mr-3" />
+                <div>
+                  <h2>Help & support</h2>
+                  <h2>support@home.com</h2>
+                </div>
+              </span>
+
+            </div>
+
+          </div>
+
         </div>
       </div>
     </div>
